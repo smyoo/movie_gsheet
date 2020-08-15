8b8668b5dcb4e9acb792a3154b4475110e5170c1
Yu Seungmin <smyoo@toss.im>
2020-08-15 12:45:31 +0900

    .

--- test.py
+++ test.py
@@ -6,7 +6,6 @@ pp = pprint.PrettyPrinter(indent=4)
 key = "7d6adcbd79ec916a9d400b3f76f3ddfd"
 prdtyear = "1958"
 url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key={0}&prdtStartYear={1}&prdtEndYear={1}&itemPerPage=100&curPage=1".format(key, prdtyear)
-print(url)
 #url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key={0}&prdtStartYear=1956&prdtEndYear=1956&itemPerPage=100&curPage=2".format(key)
 #url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key={0}&prdtStartYear=1956&prdtEndYear=1956".format(key)
 
@@ -26,4 +25,6 @@ pp.pprint(result)
 movie_list = result['movieListResult']['movieList']
 print(len(movie_list))
 print(">>> 총 영화수 : {0}".format(result['movieListResult']['totCnt']))
+
+safdf
 
